9223b115add42777253c983fa2fe7177178fc1cc
Nuhu Ainul Islam <9663525@gmail.com>
2024-03-15 23:14:33 +0600

    Update all.js

--- all.js
+++ all.js
@@ -4,4 +4,4 @@ alert('./all.js is connected, bro!');
 // day-3: dated 2024-03-11 
 // coded in the "private-repo" to add a few timers to track some personal event tracking, initially taken help from OpenAI and then re-coded as per my needs 
 console.log('day-3 successful');
-// day-7: couldn't code anything, so commiting a comment 
+// day-7: couldn not code anything, so commiting a comment 
