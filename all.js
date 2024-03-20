bda274dfd96239d62df3f5b27fc910a29f308dbb
Nuhu Ainul Islam <9663525@gmail.com>
2024-03-20 19:43:21 +0600

    Update all.js

--- all.js
+++ all.js
@@ -9,5 +9,5 @@ console.log('day-3 successful');
 // date: 2024-03-17 working on ph b9a7 project 
 // date: 2024-03-18 worked on ph b9a7 project. 83% could be done.  
 // date: 2024-03-19 
-console.log('today is 18th March of 2024. try to build a template in this repo to organize your ph learning journal.');
+console.log('today is 19th March of 2024. try to build a template in this repo to organize your ph learning journal.');
 console.log('today is 20th March of 2024. yet to try to build a template in this repo to organize your ph learning journal. sad');
